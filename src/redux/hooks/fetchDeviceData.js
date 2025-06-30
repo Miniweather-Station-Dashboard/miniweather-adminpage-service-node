8b42875a14351f3befd0220faf284e3266daf488
inamnurulf <aam.inam7310@gmail.com>
2025-07-01 04:01:10 +0700

    feat: refactor device data fetching to use apiClient for improved API interaction

--- src/redux/hooks/fetchDeviceData.js
+++ src/redux/hooks/fetchDeviceData.js
@@ -7,28 +7,27 @@ import {
   setError,
   setTotalCount,
 } from "../slices/deviceSlice";
+import apiClient from "@/lib/apiClient";
 
 export async function fetchDevice(dispatch) {
   dispatch(setStatus("loading"));
   dispatch(setError(null));
 
   try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/onboarding-device/admin`
-    );
-    const result = await response.json();
-
-    if (result.status === "success") {
+    console.log("Fetching devices from API...");
+    const result = await apiClient.get(`/v1/onboarding-device/admin`);
+    
+    if (result.data.status === "success") {
       let deviceList = [];
 
-      if (Array.isArray(result.data.devices)) {
-        deviceList = result.data.devices;
-      } else if (result.data.device) {
-        deviceList = [result.data.device];
+      if (Array.isArray(result.data.data.devices)) {
+        deviceList = result.data.data.devices;
+      } else if (result.data.data.device) {
+        deviceList = [result.data.data.device];
       }
 
       dispatch(setDeviceList(deviceList));
-      dispatch(setTotalCount(result.data.totalCount || 0));
+      dispatch(setTotalCount(result.data.data.totalCount || 0));
       dispatch(setActiveDevice(deviceList[0] || null));
       dispatch(setStatus("succeeded"));
     } else {
